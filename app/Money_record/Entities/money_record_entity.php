2ebe00df776171f5d09f284749a6d447244effa2
Ifalo <mailforjob6910@gmail.com>
2016-08-20 00:06:08 +0800

    刪除不必要的註解

--- app/Money_record/Entities/money_record_entity.php
+++ app/Money_record/Entities/money_record_entity.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class money_record_entity extends Model
 {
-    /**
-     * 與模型關聯的資料表。
-     *
-     * @var string
-     */
+
     protected $table = 'money_record';
     public $timestamps = false;
 
