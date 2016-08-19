957f1550b7b5d30aaf0863d0cd54886fd573f665
Ifalo <ifalo@Ifalode-MacBook-Pro.local>
2016-08-19 18:24:19 +0800

    微修改

--- app/Member/Services/Member_Services.php
+++ app/Member/Services/Member_Services.php
@@ -2,29 +2,29 @@
 
 namespace App\Member\Services;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Member\Entities\Member_Entity;
 
 class Member_Services
 {
     function verify()
     {
-        return $this->where('Account',$_POST['Account'])->get();
+        return Member_Entity::where('Account',$_POST['Account'])->get();
     }
     function increase()
     {
-        return $this->insert(['Account'=>$_POST['Account'],'Password'=>md5($_POST['Password'])]);
+        return Member_Entity::insert(['Account'=>$_POST['Account'],'Password'=>md5($_POST['Password'])]);
     }
     function modify()
     {
-        return $this->where('Account',$_POST['Account'])->update(['Password'=>md5($_POST['Password'])]);
+        return Member_Entity::where('Account',$_POST['Account'])->update(['Password'=>md5($_POST['Password'])]);
     }
     function delete()
     {
-        return $this->where('Account','=',$_POST['Account'])->delete();
+        return Member_Entity::where('Account','=',$_POST['Account'])->delete();
     }
     function login()
     {
-        return $this->where('Account',$_POST['Account'])->where('Password',md5($_POST['Password']))->get();
+        return Member_Entity::where('Account',$_POST['Account'])->where('Password',md5($_POST['Password']))->get();
     }
 
 }
