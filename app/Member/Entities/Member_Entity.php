37a0910657ea29fdbc491479ff2ad764d8bf6b88
Ifalo <mailforjob6910@gmail.com>
2016-08-20 00:05:29 +0800

    刪除不必要的註解

--- app/Member/Entities/Member_Entity.php
+++ app/Member/Entities/Member_Entity.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member_Entity extends Model
 {
-    /**
-     * 與模型關聯的資料表。
-     *
-     * @var string
-     */
+
     protected $table = 'member_data';
     public $timestamps = false;
 
